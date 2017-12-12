5513bbf1a615e6a9a9db8a661a70dcc871621ba2
admin <admin@pc-b139.local>
2017-12-12 14:50:29 +0900

    modify layouts

--- Todays_Swap/ViewController.swift
+++ Todays_Swap/ViewController.swift
@@ -11,18 +11,42 @@ import Alamofire
 import SwiftyJSON
 import Fuzi
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource {
 
 
     @IBOutlet weak var BuySwapLabel: UILabel!
     @IBOutlet weak var SellSwapLabel: UILabel!
     @IBOutlet weak var AddDateLabel: UILabel!
     
+    @IBOutlet weak var SumMoneyLabel: UILabel!
     
+    @IBOutlet weak var MyPickerView: UIPickerView!
+    
+    
+    let list = ["10000", "20000", "30000", "40000", "50000"]
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return list[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let kendama = Float64(list[row])!/100.0
+        SumMoneyLabel.text = String(kendama*Float64(BuySwapLabel.text!)!)
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        MyPickerView.delegate = self
+        MyPickerView.dataSource = self
+        BuySwapLabel.text = "0"
+        SellSwapLabel.text = "0"
         // Do any additional setup after loading the view, typically from a nib.
         Alamofire.request("https://www.moneypartners.co.jp/market/swap/nano/", method: .get).responseString(completionHandler: {response in
             // ここに処理を記述していく
@@ -54,6 +78,7 @@ class ViewController: UIViewController {
                         self.BuySwapLabel.text = td[23].stringValue
                         self.SellSwapLabel.text = td[28].stringValue
                         
+                        self.SumMoneyLabel.text = String(Float64(self.BuySwapLabel.text!)!*100)
                         
                     }
                 }
